8884af066319ce7b1b22b5d3c7943620c9de2304
Tadeo <domenechtadeo@outlook.es>
2022-01-28 18:12:31 +0100

    Create the Header component and added in App

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div>
+      <h1 className="display-2 mt-5 text-center">Search your album</h1>
+      <hr />
+    </div>
+  );
+};
