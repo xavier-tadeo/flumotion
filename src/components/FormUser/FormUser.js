afd5972061d772c2930a8ec20e2acac16a1984eb
Tadeo <domenechtadeo@outlook.es>
2022-01-28 19:45:40 +0100

    Added in component FormUser hook and handleSubmit

--- src/components/FormUser/FormUser.js
+++ src/components/FormUser/FormUser.js
@@ -1,9 +1,17 @@
-import React from "react";
+import { useUserForm } from "../../hooks/useUserForm";
 
 export const FormUser = () => {
+  const { handleChange, inputValue, initialValue, setInputValue } =
+    useUserForm();
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    setInputValue(initialValue);
+  };
+
   return (
     <div className="d-flex justify-content-center">
-      <form className="container row">
+      <form className="container-form row" onSubmit={handleSubmit}>
         <label htmlFor="title" className="form-label text-center p-2 display-6">
           Title
         </label>
@@ -12,6 +20,8 @@ export const FormUser = () => {
           id="title"
           className="form-control text-center"
           placeholder="Type to search..."
+          value={inputValue.title}
+          onChange={handleChange}
         />
         <label
           htmlFor="author"
@@ -21,10 +31,15 @@ export const FormUser = () => {
         </label>
         <input
           type="text"
-          id="title"
+          id="author"
           className="form-control text-center"
           placeholder="Type to search..."
+          value={inputValue.author}
+          onChange={handleChange}
         />
+        <div className="d-grid gap-2 col-4 mx-auto">
+          <button className="btn btn-success mt-4 btn-sm ">Add</button>
+        </div>
       </form>
     </div>
   );
