52a3ad32116be73a3553ed77d40c1c3bce63056a
Tadeo <domenechtadeo@outlook.es>
2022-01-29 10:28:32 +0100

    Added MainPage a App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import { FormUser } from "./components/FormUser/FormUser";
-import { Header } from "./components/Header/Header";
+import { MainPage } from "./components/MainPage/MainPage";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <FormUser />
+      <MainPage />
     </div>
   );
 }
